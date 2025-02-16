244c840bef58739386d674a9b240cd913c601fdf
Rubienus <167700445+Rubienus@users.noreply.github.com>
2025-02-16 22:40:45 +0800

    page 1 change

--- flutter_ecotracker/lib/main.dart
+++ flutter_ecotracker/lib/main.dart
@@ -117,23 +117,57 @@ class HomePage extends StatelessWidget {
 
 class Page1 extends StatelessWidget {
   const Page1({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text('Page 1 Content'),
-        Divider(
-          color: Colors.black,
-          height: 10,
-          thickness: 0.5,
-          indent: 25,
-          endIndent: 25,
-        ),
-        Text('Page 1 Content'),
-        Text('Page 1 Content'),
-        Text('Page 1 Content'),
-        Text('Page 1 Content'),
-      ],
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return Card(
+          margin: EdgeInsets.all(10),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/profile_picture.png'),
+                    ),
+                    SizedBox(width: 10),
+                    Text('User Name'),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text('This is a sample blog post content. It can be a few lines long.'),
+                SizedBox(height: 10),
+                Image.asset('assets/sample_image.png'),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.thumb_up),
+                        SizedBox(width: 5),
+                        Text('Like'),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.comment),
+                        SizedBox(width: 5),
+                        Text('Comment'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
