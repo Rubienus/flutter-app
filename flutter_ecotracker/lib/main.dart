8a5005061775d102b0ea051618a01ef89234dfff
Rubienus <167700445+Rubienus@users.noreply.github.com>
2025-02-22 16:18:27 +0800

    added notif page and added page folder

--- flutter_ecotracker/lib/main.dart
+++ flutter_ecotracker/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'nearyou_page.dart';
-import 'events_page.dart';
+import 'pages/nearyou_page.dart';
+import 'pages/events_page.dart';
+import 'pages/notifications_page.dart';
 
 const primaryColor = Color(0xFF91A287);
 const secondaryColor = Color(0xFFcbb89d);
@@ -25,94 +26,98 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    NearyouPage(),
+    EventsPage(),
+    NotificationsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: primaryColor,
-          centerTitle: true,
-          leading: Builder(
-            builder: (context) {
-              return IconButton(
-                icon: Icon(Icons.account_circle),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-              );
-            },
-          ),
-          title: const Text('Ecotracker'),
-          bottom: TabBar(
-            dividerHeight: 1.5,
-            labelColor: Colors.white,
-            indicatorColor: Colors.white,
-            tabs: [
-              Tab(text: 'For you'),
-              Tab(text: 'Events'),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        centerTitle: true,
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: Icon(Icons.account_circle),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
         ),
-        body: TabBarView(
-          children: [
-            NearyouPage(),
-            EventsPage(),
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: tertiaryColor,
-                ),
-                child: Text('Ecotracker'),
+        title: const Text('Ecotracker'),
+      ),
+      body: _pages.elementAt(_selectedIndex),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: tertiaryColor,
               ),
-              ListTile(
-                title: Text('Profile'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ProfilePage()),
-                  );
-                },
-              ),
-              ListTile(
-                title: Text('Coupons'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => CouponsPage()),
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: primaryColor,
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.white,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+              child: Text('Ecotracker'),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add_circle),
-              label: 'Post',
+            ListTile(
+              title: Text('Profile'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProfilePage()),
+                );
+              },
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.notifications),
-              label: 'Notifications',
+            ListTile(
+              title: Text('Coupons'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CouponsPage()),
+                );
+              },
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: primaryColor,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle),
+            label: 'Post',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+        ],
       ),
     );
   }
@@ -173,3 +178,4 @@ class CouponsPage extends StatelessWidget {
     );
   }
 }
+
